6f0aea61707e627735f7e288359e764fa41ab176
Hoa Le <Hoa_le9485@yahoo.com>
2021-11-28 00:34:28 -0500

    review

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart , setIsInCart] = useState(false)
+
+  function handleCartAddClick(){
+    setIsInCart((isInCart) => !isInCart)
+  }
+const cart = isInCart ? "in-cart" : ""
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isInCart ? "remove" : "add"} onClick={handleCartAddClick}>{isInCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
